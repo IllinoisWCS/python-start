21eda434f4f78c39dde6cebcf5b9f69f45ef0f15
berwin <berwin7996@gmail.com>
2016-08-25 17:46:12 -0500

    added intermediate test and advanced barebone

--- advanced.py
+++ advanced.py
@@ -3,3 +3,6 @@ This time, let's pick our own csv data sets and visualize them!
 https://catalog.data.gov/dataset?res_format=CSV
 '''
 
+'''
+
+'''
--- intermediate_tester.py
+++ intermediate_tester.py
@@ -7,8 +7,15 @@ from intermediate import Intermediate as intermediate
 # This creates an object with all the functions we created
 b = intermediate()
 
-class TestBasicMethods(unittest.TestCase):
-
+class TestIntermediateMethods(unittest.TestCase):
+    def test_dictionary(self):
+        book_index = {
+            apples: [2, 5, 64, 66, 70],
+            oranges: [3, 6, 63, 64, 70],
+            grapes: [3, 4, 5, 50, 64]
+        }
+        self.assertTrue(b.dictionary_exercise(book_index, 'apples', 'oranges') == [64,70])
+        print('Dictionary Success')
 
 if __name__ == '__main__':
         unittest.main()

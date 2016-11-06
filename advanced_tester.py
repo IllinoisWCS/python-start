604ae775b7a43d0b8405724df7b408aa29ac1f61
Emily Chao <elchao96@live.com>
2016-11-06 14:54:28 -0600

    finished jupyter conversion

--- advanced_tester.py
+++
@@ -1,7 +0,0 @@
-from advanced import Advanced as advanced
-# This creates an object with all the functions we created
-b = advanced()
-
-b.graph_data()
-
-# Feel free to test more functions here
--- json_example_tester.py
+++
@@ -1,5 +0,0 @@
-from json_example import JSONExample
-# This creates an object with all the functions we created
-j = JSONExample()
-
-j.graph_data_histogram()
